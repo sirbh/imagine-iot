310638c403a913c9c69257bd0d8207ebc238943d
saurabh chauhan <sourabh1202@gmail.com>
2024-06-11 13:04:38 +0300

    added input box with btn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,58 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  TextField,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Home() {
+  useEffect(() => {
+    // Update the document title using the browser API
+    axios.get("/api/hello").then((response) => {
+      console.log(response.data);
+    }
+    );
+    
+  }, []);
+
+  const [input, setInput] = useState("");
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <>
+      <AppBar position="static" sx={{ width: "100%" }}>
+        <Toolbar sx={{ width: "100%" }}>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            IMAGINE-IoT AskMe service
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box
+        sx={{
+          width: "100%",
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Box sx={{display:'flex', alignContent:'center', justifyContent:"center"}}>
+        <TextField label="Ask Me About Your City" sx={{ width: "500px" }} value={input} onChange={(event)=>{
+          setInput(event.target.value);
+          console.log(event.target.value);
+        }} />
+        <Button variant="contained" color="primary" onClick={()=>{console.log("clicked")} }>Ask</Button>
+        </Box>
+      </Box>
+    </>
   );
 }
